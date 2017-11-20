ff2b633c3c50d2a892fdbf09b8946978725a3cfd
Squabbi Kong <squabbi@outlook.com>
2017-11-20 19:41:16 +1100

    Removed AddFactoryImage

--- DogeKit Pixel 2/Models/Device.cs
+++ DogeKit Pixel 2/Models/Device.cs
@@ -14,11 +14,6 @@ namespace DogeKit_Pixel_2.Models
         [XmlElement]
         public ObservableCollection<TWRP> RecoveryVersions = new ObservableCollection<TWRP>();
 
-        public void AddFactoryImage(string build, string releaseId, string version)
-        {
-            FactoryImages.Add(new FactoryImage() { Build = build, ReleaseId = releaseId, Version = version });
-        }
-
         public override string ToString()
         {
             return Name + " (" + Codename + ")";
