50f6774387e31714257492334dca04638e24dc9a
Squabbi Kong <squabbi@outlook.com>
2017-11-20 19:37:03 +1100

    Fixed namespace

--- DogeKit Pixel 2/Models/TWRP.cs
+++ DogeKit Pixel 2/Models/TWRP.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Serialization;
+﻿using System.Xml.Serialization;
 
-namespace DogeKit_Serializer.Models
+namespace DogeKit_Pixel_2.Models
 {
     public class TWRP
     {
